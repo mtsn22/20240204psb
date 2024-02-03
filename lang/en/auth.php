dc27d2dbf884e86d2195c1051ccab7e8dd5975d6
tsnadmin <121678992+mtsn22@users.noreply.github.com>
2024-02-04 06:44:42 +0700

    Initial commit

---
+++ lang/en/auth.php
@@ -0,0 +1,21 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Authentication Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are used during authentication for various
+    | messages that we need to display to the user. You are free to modify
+    | these language lines according to your application's requirements.
+    |
+    */
+
+    'failed' => 'Nomor KK belum terdaftar',
+    'password' => 'Tidak ada data ananda yang dapat naik qism',
+    'throttle' => 'Too many login attempts. Please try again in :seconds seconds.',
+    'bukanpendaftar' => 'Nomor KK bukan pendaftar santri baru',
+
+];
